5284c0c8afb93f9202cc1d484a3c4efad69c9589
zpv2jdfc <zpv2jdfc@gmail.com>
2022-10-20 18:05:54 +0800

    添加搜索框

---
+++ src/components/LargeHeader/LeftEntry.vue
@@ -0,0 +1,100 @@
+<template>
+    <ul>
+        <li >
+            <a class="icon-home" href="https:/www.bilibili.com" target="_blank">
+                <BiliIcon></BiliIcon>
+                <div class="mini-header-title">
+                    <span>首页</span>
+                </div>
+            </a>
+        </li>
+        <li class="default-li">
+            <a href="/" target="_blank">
+                <span>番剧</span>
+            </a>
+        </li>
+        <li class="default-li">
+            <a href="/" target="_blank">
+                <span>直播</span>
+            </a>
+        </li>
+        <li class="char-4-li">
+            <a href="/" target="_blank">
+                <span>游戏中心</span>
+            </a>
+        </li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+import BiliIcon from '../icons/BiliIcon.vue'
+</script>
+
+<style scoped>
+ul{
+    width: 608px;
+    height: 64px;
+    list-style: none;
+}
+.default-li{
+    float: left;
+    width: 43px;
+    height: 64px;
+}
+.default-li a{
+    width: 28px;
+    height: 56px;
+    margin: 0 15px 0 0;
+    align-items: center;
+    line-height: 64px;
+    display: inline-block;
+}
+.char-4-li{
+    float: left;
+    width: 71px;
+    height: 64px;
+}
+.char-4-li a{
+    width: 56px;
+    height: 64px;
+    margin: 0 15px 0 0;
+    align-items: center;
+    line-height: 64px;
+    display: inline-block;
+}
+ul li:first-child{
+    float: left; 
+    width: 148px; 
+    height: 56px;
+}
+.icon-home{
+    margin: 0 20px 0 0;
+    width: 128px;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    /* justify-content: space-between;
+    margin-right: 20px;
+    height: 56px; */
+}
+.icon-home svg{
+    margin: 0 25px 0 0;
+    width: 60px;
+    height: 64px;
+    fill: #1ABDE6;
+    /* float: left; */
+}
+.icon-home div{
+    width: 43px;
+    height: 20px;
+    /* float: left; */
+}
+.icon-home .mini-header-title{
+    display: flex;
+    align-items: center;
+    text-align: center;
+    font-size: 14px;
+    line-height: 20px;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/common/CenterSearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="center-search-bar">
+        <form style="border-radius: 8px;">
+            <div class="nav-search-content">
+                <input class="nav-search-input" type="text" autocomplete="off" accesskey="s" maxlength="100" x-webkit-speech="" x-webkit-grammar="builtin:translate" value="" placeholder="电影解说">
+            </div>
+            <div class="nav-search-btn">
+                <SearchBtn></SearchBtn>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SearchBtn from '../icons/SearchBtn.vue';
+</script>
+
+<style scoped>
+.center-search-bar{
+    width: 362px;
+    height: 40px;
+}
+</style>
